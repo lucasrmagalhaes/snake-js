72a8161875e9593b9ff7f95c7b37a31a500ca5af
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-07-25 18:10:04 -0300

    Criação da Cobrinha.

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); 
 // 32 pixels cada quadrado.
 let box = 32;
+let snake = []; // Criando um array.
+
+// Passando o que vai ter dentro do array.
+snake[0] = { // Definindo a posição.
+    x: 8 * box, // Dando um tamanho.
+    y: 8 * box
+}
 
 // Criando a função do Background.
 function criarBG(){
@@ -13,5 +20,16 @@ function criarBG(){
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha(){
+    // for vai percorrer todo o tamanho do array e vai incrementar.
+    for(i=0; i < snake.length; i++){
+        // Definindo a cor.
+        context.fillStyle = "green";
+        // Passando o tamanho.
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
 
